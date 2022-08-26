4bf8acfa8bbf48ce957501853c3939a0e5d186af
Aleksei <Ffl.B-Unit@mail.ru>
2022-08-26 19:12:01 +0300

    Step3: download data in PostgreSQL and protect done

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-xport FLASK_APP=application
+export FLASK_APP=application
 export FLASK_ENV=development
 flask run
 
-user: postgres
-https://unixhow.com/4679/ustanovka-i-zapusk-postgresql-na-os-manjaro
 https://techviewleo.com/install-postgresql-on-arch-manjaro-garuda-linux/
+https://wiki.archlinux.org/title/PostgreSQL#Installation
+
+systemctl status postgresql
+systemctl start postgresql
 
 ip 127.0.0.1
 port 5432
 user zuba
 password jw8s0F4
 database my_db
+
+
+DuplicateTable
--- application.py
+++ application.py
@@ -3,13 +3,14 @@ from flask import (
     render_template,
     request,
 )
-import csv
 from copy import deepcopy
 from psycopg2 import Error, connect
+import csv
 
 app = Flask(__name__)
 
 
+# Work with PostgreSQL
 def connect_db():
     try:
         conn = connect(
@@ -32,24 +33,58 @@ def disconnect_db(connection, cursor):
     connection.close()
 
 
-def get_csv_content(file_path: str) -> iter:
+def create_table(name: str):
+    connection, cursor = connect_db()
+    table = f'''CREATE TABLE {name} (
+        ID SERIAL PRIMARY KEY,
+        date   VARCHAR,
+        vehicle   VARCHAR,
+        vehicle_number   VARCHAR,
+        square   VARCHAR,
+        scope_of_work   VARCHAR
+    );'''
+    cursor.execute(table)
+    connection.commit()
+    disconnect_db(connection, cursor)
+
+
+def write_data_to_db(data: list, name: str):
+    connection, cursor = connect_db()
+    for line in data:
+        ins_data = f"""INSERT INTO {name} 
+        (date, vehicle, vehicle_number, square, scope_of_work) VALUES (
+            '{line["Дата"]}',
+            '{line["Техника"]}',
+            '{line["Номер техники"]}',
+            '{line["Площадь, га"]}',
+            '{line["Объем работ, м3"]}'
+        );"""
+
+        cursor.execute(ins_data)
+    connection.commit()
+    disconnect_db(connection, cursor)
+
+
+# Work with CSV data
+def get_csv_data(file_path: str) -> iter:
     with open(file_path, encoding="utf-8-sig") as content:
         return list(csv.DictReader(content))
 
 
 def normalize_csv_data(volume_path: str, machine_path: str) -> list:
-    volume_raw = get_csv_content(volume_path)
-    machine_raw = get_csv_content(machine_path)
+    volume_raw = get_csv_data(volume_path)
+    machine_raw = get_csv_data(machine_path)
     result_data = deepcopy(machine_raw)
     for item in result_data:
         for i in range(len(machine_raw)):
 
-            if item["Data"] == volume_raw[i]["Data"]:
+            if item["Дата"] == volume_raw[i]["Дата"]:
                 item.update(volume_raw[i])
 
     return result_data
 
 
+# Work with HTML
 @app.route('/')
 def root():
     return render_template('index.html')
@@ -63,6 +98,12 @@ def load_data():
         volume = request.form['volume']
         machine = request.form['machine']
         data_for_db = normalize_csv_data(volume, machine)
+        try:
+            create_table(name)
+        except (Exception, Error):
+            pass
+        finally:
+            write_data_to_db(data_for_db, name)
 
     return render_template('load.html')
 
